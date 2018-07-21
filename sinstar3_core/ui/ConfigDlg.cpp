80ce99c2f6094710c777044dd3f6eca9f9b5ace9
008 <359501300@qq.com>
2018-07-21 16:58:36 +0800

    初步实现设置对话框，菜单改成smenuex。加上半透效果

--- sinstar3_core/ui/ConfigDlg.cpp
+++ sinstar3_core/ui/ConfigDlg.cpp
@@ -3,7 +3,7 @@
 
 namespace SOUI
 {
-	CConfigDlg::CConfigDlg(void):SHostDialog(UIRES.LAYOUT.dlg_config)
+	CConfigDlg::CConfigDlg(void) :SHostDialog(UIRES.LAYOUT.dlg_config)
 	{
 	}
 
@@ -11,17 +11,300 @@ namespace SOUI
 	{
 	}
 
-	//CSettingsLocal		g_SettingsL;
-	//CSettingsCompSpec   g_SettingsCompSpec;
-	
-	void CConfigDlg::UpdataCtrl()
+	void CConfigDlg::FindAndSetCheck(int id,BOOL bcheck)
 	{
-
+		SWindow *pCtrl = FindChildByID(id);
+			SASSERT(pCtrl);
+			if (pCtrl)
+				pCtrl->SetCheck(bcheck);
 	}
 
-	BOOL CConfigDlg::OnInitDialog( HWND wnd, LPARAM lParam )
+	void CConfigDlg::FindAndSetText(int id,LPCTSTR text)
 	{
-		UpdataCtrl();
+		SWindow *pCtrl = FindChildByID(id);
+			SASSERT(pCtrl); 
+			if (pCtrl)
+				pCtrl->SetWindowText(text);
+	}
+
+	void CConfigDlg::FindAndSetHotKey(int id,WORD vk_key, WORD modif)
+	{
+		SHotKeyCtrl *pCtrl = FindChildByID2<SHotKeyCtrl>(id);
+			SASSERT(pCtrl);
+			if (pCtrl)
+				pCtrl->SetHotKey(vk_key, modif);
+	}
+
+	void CConfigDlg::IniPageHabit()
+	{
+		int CtrlId;
+		CtrlId = 102;
+		//输入法开关
+		if (g_SettingsG.bySwitchKey == 0x36)
+			CtrlId = 100;
+		else if (g_SettingsG.bySwitchKey == 0x2a)
+			CtrlId = 101;
+		FindAndSetCheck(CtrlId,TRUE);
+
+		//形码状态回车
+		CtrlId = g_SettingsG.bEnterClear ? 110 : 111;
+		FindAndSetCheck(CtrlId, TRUE);
+		//形码状态快捷模式		
+		FindAndSetCheck(R.id.key_to_umode, g_SettingsG.bFastUMode);
+		//临时拼音开关
+		CtrlId = 122;
+		switch (g_SettingsG.byTempSpellKey)
+		{
+		case 0xc0:CtrlId = 120; break;
+		case 0xc1:CtrlId = 121; break;
+		default:g_SettingsG.byTempSpellKey = 0; break;
+		}
+		FindAndSetCheck(CtrlId, TRUE);
+		//重码自动上屏
+		FindAndSetCheck(R.id.cand_auto_input, g_SettingsG.bAutoInput);
+		//拼音重码词组优先
+		FindAndSetCheck(R.id.cand_py_phrase_first, g_SettingsG.bPYPhraseFirst);
+	}
+
+	void CConfigDlg::IniPageHotKey()
+	{
+		FindAndSetHotKey(R.id.hk_switch_py, g_SettingsG.byHotKeyMode, MOD_CONTROL);
+		FindAndSetHotKey(R.id.hk_make_phrase, g_SettingsG.byHotKeyMakeWord, MOD_CONTROL);
+		FindAndSetHotKey(R.id.hk_show_table, g_SettingsG.byHotKeyShowRoot, MOD_CONTROL);
+		FindAndSetHotKey(R.id.hk_show_comp, g_SettingsG.byHotKeyQuery, MOD_CONTROL);
+		FindAndSetHotKey(R.id.hk_show_statusbar, g_SettingsG.byHotKeyHideStatus, MOD_CONTROL);
+		FindAndSetHotKey(R.id.hk_input_en, g_SettingsG.byHotKeyEn, MOD_CONTROL);
+	}
+
+	void CConfigDlg::IniPageAssociate()
+	{
+		int CtrlId;
+		//联想
+		CtrlId = 400+g_SettingsG.byAstMode;
+		FindAndSetCheck(CtrlId, TRUE);
+		CtrlId=500+g_SettingsG.byRateAdjust;
+		FindAndSetCheck(CtrlId, TRUE);
+		CtrlId = 600;
+		if (g_SettingsG.byForecast == MQC_FORECAST)
+			CtrlId = 601;
+		else if (g_SettingsG.byForecast == MQC_FCNOCAND)
+			CtrlId = 602;
+		FindAndSetCheck(CtrlId, TRUE);
+
+		FindAndSetCheck(R.id.chk_auto_comp_promp, g_SettingsG.bAutoPrompt);
+		FindAndSetCheck(R.id.chk_auto_dot, g_SettingsG.bAutoDot);
+		FindAndSetCheck(R.id.chk_auto_select_cand, g_SettingsG.bAutoMatch);
+		FindAndSetCheck(R.id.chk_sent_input, g_SettingsG.bAstSent);
+	}
+
+	//win+
+	/*SStringT MakeHotKeyString(BYTE vk_value)
+	{
+		TCHAR KeyName[66];
+		GetKeyNameText(MapVirtualKey(vk_value, 0) << 16,KeyName,66);
+		if (vk_value >= 0x30) return KeyName;
+		else return SStringT(_T("win+")) + KeyName;
+	}*/
+
+	void CConfigDlg::IniPageCandidate()
+	{
+		FindAndSetCheck(R.id.chk_enable_23cand_hotkey, g_SettingsG.b23CandKey);
+
+		FindAndSetHotKey(R.id.hk_2_cand, g_SettingsG.by2CandVK, g_SettingsG.by2CandVK >= 0x30 ? 0 : MOD_WIN);
+		FindAndSetHotKey(R.id.hk_3_cand, g_SettingsG.by3CandVK, g_SettingsG.by3CandVK >= 0x30 ? 0 : MOD_WIN);
+		
+		FindAndSetHotKey(R.id.hk_turn_prev, g_SettingsG.byTurnPageUpVK, g_SettingsG.byTurnPageUpVK >= 0x30 ? 0 : MOD_WIN);
+		FindAndSetHotKey(R.id.hk_turn_next, g_SettingsG.byTurnPageDownVK, g_SettingsG.byTurnPageDownVK >= 0x30 ? 0 : MOD_WIN);
+		
+		FindAndSetCheck(R.id.chk_disable_number_to_select_cand, g_SettingsG.bCandSelNoNum);
+		FindAndSetCheck(R.id.chk_full_skip_simple, g_SettingsG.bOnlySimpleCode);
+
+		FindAndSetCheck(700 + g_SettingsG.nGbkMode, TRUE);
+	}
+	
+	void CConfigDlg::IniCtrl()
+	{		
+		IniPageHabit();
+		IniPageHotKey();
+		IniPageAssociate();	
+		IniPageCandidate();
+	}
+
+	BOOL CConfigDlg::OnInitDialog(HWND wnd, LPARAM lParam)
+	{
+		ImmAssociateContext(m_hWnd, (HIMC)NULL);
+
+		IniCtrl();
 		return FALSE;
+	}
+
+#define GetGroupCheck(id) int CheckId=0;\
+SWindow *pCtrl = FindChildByID(id);\
+	SASSERT(pCtrl);\
+	pCtrl=pCtrl->GetSelectedSiblingInGroup();\
+	SASSERT(pCtrl);\
+	CheckId=pCtrl->GetID()
+
+	void CConfigDlg::OnClickInputSwitch(int id)
+	{
+		GetGroupCheck(id);
+		switch (CheckId)
+		{
+		case 100:
+			g_SettingsG.bySwitchKey = 0x36;
+			break;
+		case 101:
+			g_SettingsG.bySwitchKey = 0x2a;
+			break;
+		default:g_SettingsG.bySwitchKey = 0; break;
+		}
+	}
+
+	void CConfigDlg::OnClickEnter(int id)
+	{
+		GetGroupCheck(id);
+		switch (CheckId)
+		{
+		case 110:
+			g_SettingsG.bEnterClear=TRUE;
+			break;
+		case 111:
+			g_SettingsG.bEnterClear = FALSE;
+			break;
+		default:
+			g_SettingsG.bEnterClear = FALSE;
+			break;
+		}
+	}
+
+	void CConfigDlg::OnClickPYTemp(int id)
+	{
+		GetGroupCheck(id);
+		switch (CheckId)
+		{
+		case 120:
+			g_SettingsG.byTempSpellKey = 0xC0;
+			break;
+		case 121:
+			g_SettingsG.byTempSpellKey = 0xC1;
+			break;
+		default:
+			g_SettingsG.byTempSpellKey = FALSE;
+			break;
+		}
+	}
+
+	void CConfigDlg::OnAutoInput()
+	{
+		g_SettingsG.bAutoInput=FindChildByID(R.id.cand_auto_input)->IsChecked();
+	}
+
+	void CConfigDlg::OnPyPhraseFirst()
+	{
+		g_SettingsG.bPYPhraseFirst= FindChildByID(R.id.cand_py_phrase_first)->IsChecked();
+	}
+
+	void CConfigDlg::OnClickAssMode(int id)
+	{
+		GetGroupCheck(id);
+		g_SettingsG.byAstMode = CheckId - 400;		
+	}
+
+	void CConfigDlg::OnClickForcast(int id)
+	{
+		GetGroupCheck(id);
+
+		switch (CheckId)
+		{
+		case 600:
+			g_SettingsG.byForecast = 0;
+			break;
+		case 601:
+			g_SettingsG.byForecast = MQC_FORECAST;
+			break;
+		case 602:
+			g_SettingsG.byForecast = MQC_FCNOCAND;
+		}
+	}
+
+	void CConfigDlg::OnClickGBK(int id)
+	{
+		GetGroupCheck(id);
+
+		switch (CheckId)
+		{
+		case 700:
+			g_SettingsG.nGbkMode = CSettingsGlobal::GbkMode::GBK_HIDE;
+			break;
+		case 701:
+			g_SettingsG.nGbkMode = CSettingsGlobal::GbkMode::GBK_SHOW_MANUAL;
+			break;
+		case 702:
+			g_SettingsG.nGbkMode = CSettingsGlobal::GbkMode::GBK_SHOW_NORMAL;
+		}
+	}
+
+	void CConfigDlg::OnChkFullSkipSimple()
+	{
+		g_SettingsG.bOnlySimpleCode = FindChildByID(R.id.chk_full_skip_simple)->IsChecked();
+	}
+
+	void CConfigDlg::OnClickRateAdjust(int id)
+	{
+		GetGroupCheck(id);
+		g_SettingsG.byRateAdjust = CheckId - 500;
+	}
+	void CConfigDlg::OnClickAutoCompPromp()
+	{
+		g_SettingsG.bAutoPrompt= FindChildByID(R.id.chk_auto_comp_promp)->IsChecked();	
+	}
+	void CConfigDlg::OnClickAutoDot()
+	{
+		g_SettingsG.bAutoDot=FindChildByID(R.id.chk_auto_dot)->IsChecked();
+	}
+	void CConfigDlg::OnClickAutoSelectCand()
+	{
+		g_SettingsG.bAutoMatch = FindChildByID(R.id.chk_auto_select_cand)->IsChecked();
+	}
+	void CConfigDlg::OnClickSendInput()
+	{
+		g_SettingsG.bAstSent = FindChildByID(R.id.chk_sent_input)->IsChecked();
+	}
+	void CConfigDlg::OnDisableNumSelCand()
+	{
+		g_SettingsG.bCandSelNoNum= FindChildByID(R.id.chk_disable_number_to_select_cand)->IsChecked();
+	}
+	void CConfigDlg::OnEnable23Cand()
+	{
+		g_SettingsG.b23CandKey = FindChildByID(R.id.chk_enable_23cand_hotkey)->IsChecked();
+	}
+	void CConfigDlg::OnHotKeyEvent(EventArgs * pEvt)
+	{
+		EventSetHotKey *pHotKeyEvt = sobj_cast<EventSetHotKey>(pEvt);
+		switch (pEvt->GetID())
+		{
+			//hotpage
+		case R.id.hk_switch_py:
+			g_SettingsG.byHotKeyMode = pHotKeyEvt->vKey; break;
+		case R.id.hk_make_phrase:
+			g_SettingsG.byHotKeyMakeWord = pHotKeyEvt->vKey; break;
+		case R.id.hk_show_table:
+			g_SettingsG.byHotKeyShowRoot = pHotKeyEvt->vKey; break;
+		case R.id.hk_show_comp:
+			g_SettingsG.byHotKeyQuery = pHotKeyEvt->vKey; break;
+		case R.id.hk_show_statusbar:
+			g_SettingsG.byHotKeyHideStatus = pHotKeyEvt->vKey; break;
+		case R.id.hk_input_en:
+			g_SettingsG.byHotKeyEn = pHotKeyEvt->vKey; break;	
+			//other
+		case R.id.hk_2_cand:
+			g_SettingsG.by2CandVK = pHotKeyEvt->vKey; break;
+		case R.id.hk_3_cand:
+			g_SettingsG.by3CandVK = pHotKeyEvt->vKey; break;
+		case R.id.hk_turn_prev:
+			g_SettingsG.byTurnPageUpVK = pHotKeyEvt->vKey; break;
+		case R.id.hk_turn_next:
+			g_SettingsG.byTurnPageDownVK = pHotKeyEvt->vKey; break;
+		}
 	}
 }
--- sinstar3_core/ui/ConfigDlg.h
+++ sinstar3_core/ui/ConfigDlg.h
@@ -7,13 +7,63 @@ namespace SOUI
 	{
 	public:
 		CConfigDlg(void);
-		~CConfigDlg(void);
-
+		~CConfigDlg(void);		
 	protected:
-		void UpdataCtrl();
-		BOOL OnInitDialog(HWND wnd, LPARAM lParam);
 		
-		EVENT_MAP_BEGIN()			
+		void FindAndSetCheck(int id, BOOL bcheck);
+		void FindAndSetText(int id, LPCTSTR text);
+		void FindAndSetHotKey(int id, WORD vk_key, WORD modif);
+		void IniPageHabit();
+		void IniPageHotKey();
+		void IniPageAssociate();
+		void IniPageCandidate();
+		void IniCtrl();
+		BOOL OnInitDialog(HWND wnd, LPARAM lParam);
+		//输入法开关radio
+		void OnClickInputSwitch(int id);
+		//回车
+		void OnClickEnter(int id);
+		//临时拼音
+		void OnClickPYTemp(int id);
+		//重码自动上屏
+		void OnAutoInput();
+		//拼音重码词组优先
+		void OnPyPhraseFirst();
+		//联想类型
+		void OnClickAssMode(int id);
+		void OnClickForcast(int id);
+		//GBK重码
+		void OnClickGBK(int id);
+		//出简不出全
+		void OnChkFullSkipSimple();
+		//词频调整
+		void OnClickRateAdjust(int id);
+
+		void OnClickAutoCompPromp();
+		void OnClickAutoDot();
+		void OnClickAutoSelectCand();
+		void OnClickSendInput();
+		void OnDisableNumSelCand();
+		void OnEnable23Cand();
+		void OnHotKeyEvent(EventArgs *pEvt);
+		EVENT_MAP_BEGIN()
+			EVENT_ID_COMMAND_RANGE(100, 102, OnClickInputSwitch)
+			EVENT_ID_COMMAND_RANGE(110, 111, OnClickInputSwitch)
+			EVENT_ID_COMMAND_RANGE(120, 122, OnClickPYTemp)
+			EVENT_ID_COMMAND(R.id.cand_auto_input, OnAutoInput)
+			EVENT_ID_COMMAND(R.id.cand_py_phrase_first, OnPyPhraseFirst)
+			EVENT_ID_COMMAND_RANGE(400, 403, OnClickAssMode)
+			EVENT_ID_COMMAND_RANGE(600, 602, OnClickForcast)
+			EVENT_ID_COMMAND_RANGE(700, 702, OnClickGBK)
+			EVENT_ID_COMMAND(R.id.chk_full_skip_simple, OnChkFullSkipSimple)
+			EVENT_ID_COMMAND_RANGE(500, 502, OnClickRateAdjust)
+			EVENT_ID_COMMAND(R.id.chk_auto_comp_promp, OnClickAutoCompPromp)
+			EVENT_ID_COMMAND(R.id.chk_auto_dot, OnClickAutoDot)
+			EVENT_ID_COMMAND(R.id.chk_auto_select_cand, OnClickAutoSelectCand)
+			EVENT_ID_COMMAND(R.id.chk_sent_input, OnClickSendInput)
+			EVENT_ID_COMMAND(R.id.chk_disable_number_to_select_cand, OnDisableNumSelCand)
+			EVENT_ID_COMMAND(R.id.chk_enable_23cand_hotkey, OnEnable23Cand)
+			EVENT_HANDLER(EventSetHotKey::EventID, OnHotKeyEvent)
 		EVENT_MAP_END()
 
 		BEGIN_MSG_MAP_EX(CConfigDlg)
