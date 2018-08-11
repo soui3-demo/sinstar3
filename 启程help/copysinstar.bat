c3893f4a6cb5b8da36f14c8f061eda5b7bee27b3
setoutsoft <setoutsoft@qq.com>
2018-08-11 22:40:15 +0800

    加入2.0版本的chm帮助源文件。

---
+++ 启程help/copysinstar.bat
@@ -0,0 +1,7 @@
+copy sinstar2.chm ..\isInstallData\sinstar2
+copy sinstar2.chm "..\isInstallData for planta\sinstar2"
+copy sinstar2.chm ..\sinstar2-U\sinstar2
+copy sinstar2.chm ..\表形码-安装包\sinstar2
+copy sinstar2.chm ..\sowbInstallData\sowb\sowb.chm
+copy sinstar2.chm ..\sowb-u\sowb\sowb.chm
+copy sinstar2.chm ..\sinstar2-update
