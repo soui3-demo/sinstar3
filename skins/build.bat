a212626260a174b9d62481362a0455cfd422e135
huangjianxiong <huangjianxiong@ainemo.com>
2018-08-15 11:18:33 +0800

    更新皮肤.

--- skins/build.bat
+++ skins/build.bat
@@ -1,4 +1,9 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip "..\data\skins\windows 7.sskn" "windows 7\*" -r -mmt=on 
-"C:\Program Files\7-Zip\7z.exe" a -tzip "..\data\skins\ºÚÉ«-ÊúÅÅ.sskn" "ºÚÉ«-ÊúÅÅ\*" -r -mmt=on 
-"C:\Program Files\7-Zip\7z.exe" a -tzip "..\data\skins\´ó×Ö-À¶É«ÓÄÏã.sskn" "´ó×Ö-À¶É«ÓÄÏã\*" -r -mmt=on 
-"C:\Program Files\7-Zip\7z.exe" a -tzip "..\data\skins\´ó×Ö-×Ï÷ìÔÆ÷Ş.sskn" "´ó×Ö-×Ï÷ìÔÆ÷Ş\*" -r -mmt=on 
+cd "windows 7"
+"C:\Program Files\7-Zip\7z.exe" a -tzip "..\..\data\skins\windows 7.sskn" "*" -r -mmt=on 
+cd "..\ºÚÉ«-ÊúÅÅ"
+"C:\Program Files\7-Zip\7z.exe" a -tzip "..\..\data\skins\ºÚÉ«-ÊúÅÅ.sskn" "*" -r -mmt=on 
+cd "..\´ó×Ö-À¶É«ÓÄÏã"
+"C:\Program Files\7-Zip\7z.exe" a -tzip "..\..\data\skins\´ó×Ö-À¶É«ÓÄÏã.sskn" "*" -r -mmt=on 
+cd "..\´ó×Ö-×Ï÷ìÔÆ÷Ş"
+"C:\Program Files\7-Zip\7z.exe" a -tzip "..\..\data\skins\´ó×Ö-×Ï÷ìÔÆ÷Ş.sskn" "*" -r -mmt=on 
+cd ..
