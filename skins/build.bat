28fb14ccc6318d4d8701e1c565229c2adcd3bed4
huangjianxiong <huangjianxiong@ainemo.com>
2018-08-16 15:18:12 +0800

    皮肤增加动画支持.

--- skins/build.bat
+++ skins/build.bat
@@ -1,9 +1,9 @@
 cd "windows 7"
-"..\tools\7z.exe" a -tzip "..\..\data\skins\windows 7.sskn" "*" -r -mmt=on 
+"..\..\tools\7z.exe" a -tzip "..\..\data\skins\windows 7.sskn" "*" -r -mmt=on 
 cd "..\ºÚÉ«-ÊúÅÅ"
-"..\tools\7z.exe" a -tzip "..\..\data\skins\ºÚÉ«-ÊúÅÅ.sskn" "*" -r -mmt=on 
+"..\..\tools\7z.exe" a -tzip "..\..\data\skins\ºÚÉ«-ÊúÅÅ.sskn" "*" -r -mmt=on 
 cd "..\´ó×Ö-À¶É«ÓÄÏã"
-"..\tools\7z.exe" a -tzip "..\..\data\skins\´ó×Ö-À¶É«ÓÄÏã.sskn" "*" -r -mmt=on 
+"..\..\tools\7z.exe" a -tzip "..\..\data\skins\´ó×Ö-À¶É«ÓÄÏã.sskn" "*" -r -mmt=on 
 cd "..\´ó×Ö-×Ï÷ìÔÆ÷Ş"
-"..\tools\7z.exe" a -tzip "..\..\data\skins\´ó×Ö-×Ï÷ìÔÆ÷Ş.sskn" "*" -r -mmt=on 
+"..\..\tools\7z.exe" a -tzip "..\..\data\skins\´ó×Ö-×Ï÷ìÔÆ÷Ş.sskn" "*" -r -mmt=on 
 cd ..
