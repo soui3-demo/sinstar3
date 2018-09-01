edc2d84372f894181e27f0572adb926b2bdad0be
setoutsoft <setoutsoft@qq.com>
2018-09-01 22:32:11 +0800

    delete pleditor

--- pleditor/progcallback.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PROGCALLBACK_
-#define _PROGCALLBACK_
-
-typedef enum tagPROGTYPE{PT_MAX=0,PT_PROG,PT_END} PROGTYPE;
-typedef void (*FunProgCallback)(PROGTYPE uType,DWORD dwData,LPARAM lParam);
-
-#endif//_PROGCALLBACK_
