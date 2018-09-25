1eea0635be41d5ae41fb4a9e3ee1ad087723bb88
setoutsoft <setoutsoft@qq.com>
2018-09-25 22:46:08 +0800

    update tsf3

--- sinstar3_tsf/register.cpp
+++ sinstar3_tsf/register.cpp
@@ -26,6 +26,17 @@ static const TCHAR KTextServiceModel[] = TEXT("Apartment");
 #define TEXTSERVICE_ICON_INDEX  0
 #define	SINSTAR3_IME_FILE_NAME	_T("sinstar3_ime.ime")
 
+#if _MSC_VER < 1600
+
+/* For Windows 8 */
+const GUID GUID_TFCAT_TIPCAP_IMMERSIVESUPPORT =
+{ 0x13A016DF, 0x560B, 0x46CD, { 0x94, 0x7A, 0x4C, 0x3A, 0xF1, 0xE0, 0xE3, 0x5D } };
+
+const GUID GUID_TFCAT_TIPCAP_SYSTRAYSUPPORT =
+{ 0x25504FB4, 0x7BAB, 0x4BC1, { 0x9C, 0x69, 0xCF, 0x81, 0x89, 0x0F, 0x0E, 0xF5 } };
+
+#endif
+
 static bool IsWin8orLater()
 {
 	DWORD dwRet;
