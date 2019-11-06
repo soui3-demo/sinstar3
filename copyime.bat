9958a99bf250e2615b9f3d6ccdb2bc9792849ce4
huangjianxiong <huangjianxiong@ainemo.com>
2019-11-06 14:37:01 +0800

    remove bat.

--- copyime.bat
+++
@@ -1,6 +0,0 @@
-cd /d %~dp0
-copy data\program\sinstar3_ime.dll /y %Systemroot%\SysWOW64\sinstar3_ime.ime
-copy data\program\x64\sinstar3_ime.dll /y %Systemroot%\System32\sinstar3_ime.ime
-
-copy data\program\sinstar3_tsf.dll /y %Systemroot%\SysWOW64\sinstar3_tsf.dll
-copy data\program\x64\sinstar3_tsf.dll /y %Systemroot%\System32\sinstar3_tsf.dll
