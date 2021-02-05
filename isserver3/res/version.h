7c7582ff8a17a07b575c4bc403a6e65cc95ed020
setoutsoft <setoutsoft@qq.com>
2021-02-05 22:59:23 +0800

    版本更新到3.1.6.3

--- isserver3/res/version.h
+++ isserver3/res/version.h
@@ -10,7 +10,7 @@
 #define _sinstar3_ime_version_a		3
 #define _sinstar3_ime_version_b		1
 #define _sinstar3_ime_version_c		6
-#define _sinstar3_ime_version_d		2
+#define _sinstar3_ime_version_d		3
 
 
 #define _sinstar3_ime_int_version(a,b,c,d)		a,b,c,d
